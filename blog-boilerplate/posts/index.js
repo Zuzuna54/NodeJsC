964747b73510f508e2f4361fddb7716e73e703b3
Giorgi Giorgobiani <Giorgigiorgobiani@hotmail.com>
2024-04-04 16:14:14 -0400

    posts test app

---
+++ blog-boilerplate/posts/index.js
@@ -0,0 +1,30 @@
+// Posts service app and router 
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+
+app.get('/posts', (req, res) => {
+    console.log("Posts service get posts");
+    res.send(posts);
+
+});
+
+app.post('/posts', (req, res) => {
+    console.log("Posts service post posts");
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+    posts[id] = {
+        id, title
+    };
+    res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log('Posts service listening on port 4000');
+});
