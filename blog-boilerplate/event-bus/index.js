56d352e63de62ebc9e7be7067e1bd41c4e348aba
Giorgi Giorgobiani <Giorgigiorgobiani@hotmail.com>
2024-04-05 06:14:30 -0400

    completed app with evnet buss query service  and comment moderation service

--- blog-boilerplate/event-bus/index.js
+++ blog-boilerplate/event-bus/index.js
@@ -1,22 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', async (req, res) => {
+const events = [];
+
+app.post("/events", (req, res) => {
     const event = req.body;
 
-    await axios.post('http://localhost:4000/events', event);
-    await axios.post('http://localhost:4001/events', event);
-    await axios.post('http://localhost:4002/events', event);
+    events.push(event);
 
-    res.send({ status: 'OK' });
+    console.log("Received Event", event);
+
+    axios.post("http://localhost:4000/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post("http://localhost:4001/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post("http://localhost:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    res.send({ status: "OK" });
+    axios.post("http://localhost:4003/events", event).catch((err) => {
+        console.log(err.message);
+    });
 });
 
+app.get("/events", (req, res) => {
+    res.send(events);
+});
 
 app.listen(4005, () => {
-    console.log('Event Bus listening on port 4005');
+    console.log("Listening on 4005");
 });
