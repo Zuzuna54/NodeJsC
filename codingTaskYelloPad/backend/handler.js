885812b1827f4248f4e3d5c2698703a9b359124f
Giorgi Giorgobiani <Giorgigiorgobiani@hotmail.com>
2024-04-14 17:05:08 -0400

    deployed on serverless

--- codingTaskYelloPad/backend/handler.js
+++ codingTaskYelloPad/backend/handler.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app = require('./dist/app.js');
+const serverless = require('serverless-http');
 //import app as module from './dist/app.js';
-const awsServerlessExpress = require('aws-serverless-express');
-const server = awsServerlessExpress.createServer(app);
+// const awsServerlessExpress = require('aws-serverless-express');
+// const server = awsServerlessExpress.createServer(app);
 
 
-module.exports.latestApp = (event, context) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    awsServerlessExpress.proxy(server, event, context);
-};
+// module.exports.latestApp = (event, context) => {
+//     console.log(`EVENT: ${JSON.stringify(event)}`);
+//     awsServerlessExpress.proxy(server, event, context);
+// };
 
+module.exports.latestApp = serverless(app.default);
