21cd5338e55faa8e8eea36c2a87720718cfaa461
Giorgi Giorgobiani <Giorgigiorgobiani@hotmail.com>
2024-04-20 19:08:09 -0400

    Update anagrams.ts

--- algos/problem_solving_approach/anagrams.ts
+++ algos/problem_solving_approach/anagrams.ts
@@ -1,3 +1,39 @@
+// const isAnagram = (str1: string, str2: string): boolean => {
+
+//     if (str1.length === 0 && str2.length === 0) {
+//         return true;
+//     }
+
+//     if (str1.length !== str2.length) {
+//         return false;
+//     }
+
+
+//     let frequencyCounter1: Record<string, number> = {};
+//     let frequencyCounter2: Record<string, number> = {};
+
+//     for (let char of str1) {
+//         frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+//     }
+
+//     for (let char of str2) {
+//         frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
+//     }
+
+//     for (let key in frequencyCounter1) {
+//         if (!(key in frequencyCounter2)) {
+//             return false;
+//         }
+//         if (frequencyCounter2[key] !== frequencyCounter1[key]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+
+// }
+
+
 const isAnagram = (str1: string, str2: string): boolean => {
 
     if (str1.length === 0 && str2.length === 0) {
@@ -8,31 +44,23 @@ const isAnagram = (str1: string, str2: string): boolean => {
         return false;
     }
 
-
-    let frequencyCounter1: Record<string, number> = {};
-    let frequencyCounter2: Record<string, number> = {};
-
+    const lookup: Record<string, number> = {};
     for (let char of str1) {
-        frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+        lookup[char] = (lookup[char] || 0) + 1;
     }
+
 
     for (let char of str2) {
-        frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
-    }
-
-    for (let key in frequencyCounter1) {
-        if (!(key in frequencyCounter2)) {
+        if (!lookup[char]) {
             return false;
-        }
-        if (frequencyCounter2[key] !== frequencyCounter1[key]) {
-            return false;
+        } else {
+            lookup[char] -= 1;
         }
     }
 
-    return true;
+    return true
 
 }
-
 
 
 console.log(isAnagram('anagram', 'nararam')); //true
